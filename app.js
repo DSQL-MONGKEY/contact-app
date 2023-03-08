16c4fe6490a48e06033ec227120267ca49f6289d
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-03-08 14:49:28 +0700

    trying node file system

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import validator from 'validator';
 import * as fs from 'node:fs';
-import readline from 'readline'
+import readline from 'readline';
 
 const rl =  readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
-rl.question('Masukan nama anda: ', (nama) => {
-    rl.question('Masukan NoHP anda: ', (noHP) => {
-        console.log(`No HP anda ${noHP}`)
-        console.log(`terimakasih ${nama}, noHP anda ${noHP}`)
-        rl.close()
+rl.question('Masukkan Nama Anda: ', (nama) => {
+    rl.question('Masukkan Umur Anda: ', (umur) => {
+        rl.question('Masukan domisili anda: ', (domisili) => {
+            rl.question('Masukkan hobi: ', (hobi) => {
+                const data = {nama, umur, domisili, hobi}
+                const file = fs.readFileSync('./contacts.json', 'utf-8')
+                const contacts = JSON.parse(file)
+                
+                contacts.push(data)
+                fs.writeFileSync('./contacts.json', JSON.stringify(contacts))
+                console.log(contacts, typeof(contacts))
+                rl.close()
+            })
+        })
     })
 })
